25ab1e9d4a607d10c21f741b76fe34c0e5d4941b
David Pontes <pondavid103@gmail.com>
2024-04-10 02:32:22 -0300

    add:project run

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:conteiners/telaMod.dart';
+import 'package:flutter/material.dart';
+import 'statewidget.dart';
+import 'cabecalho.dart';
+
+void main() {
+  runApp(drawres(
+    title: 'Atividade2',
+  ));
+}
