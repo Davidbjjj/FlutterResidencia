6dc5b0bdfc3e0d7a4768b1307a1dd6f1f8fe24de
David Pontes <pondavid103@gmail.com>
2024-04-10 02:35:53 -0300

    add:Widget state

---
+++ lib/statewidget.dart
@@ -0,0 +1,13 @@
+import 'package:conteiners/main.dart';
+import 'package:conteiners/telaMod.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Conteiners extends StatefulWidget {
+  @override
+  State<Conteiners> createState() {
+    body:
+    var conteinerss = Conteiners();
+    return Conteinerss();
+  }
+}
